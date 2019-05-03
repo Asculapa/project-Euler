d0f409e95472741184932e9345e578d8047e6e8e
Olexandr <nadtochiy1915@gmail.com>
2019-05-04 00:53:32 +0300

    Update 1.py
    Changed function name

--- 1.py
+++ 1.py
@@ -1,13 +1,13 @@
 # Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
 
 
-def get_sum(number):
+def get_set(number):
     five = set(range(0, number, 3))
     three = set(range(0, number, 5))
     return five.union(three)
 
 
-print(sum(get_sum(1000)))
+print(sum(get_set(1000)))
 
 # get_sum(10) = 23
 # get_sum(1000) = 233168
